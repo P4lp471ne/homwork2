4687f450b993bee14a018475eb47c7025db7ba43
P4lp471ne <40141607+P4lp471ne@users.noreply.github.com>
2020-11-02 21:10:48 +0000

    Create client.py

---
+++ client.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import socket
+import subprocess
+import sys
+import re
+from datetime import datetime
+
+
+def measure_time(fun):
+    def inner(*args):
+        t = datetime.now()
+        fun(*args)
+        print(f"Finished in {datetime.now() - t}")
+    return inner
+
+def resolve(IP): return IP if re.fullmatch("\d+.\d+.\d+.\d+", IP) else socket.gethostbyname(IP)
+
+@measure_time
+def scan(IP, interval = (1,65535)):
+    try:
+        for port in range(*interval):  
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((remoteServer, port))
+            if result == 0:
+                print(f"Port {port} is open")
+            sock.close()
+
+    except KeyboardInterrupt:
+        print("canceled")
+        sys.exit()
+
+    except socket.error:
+        print("Couldn't connect to server")
+        sys.exit()
+
+if __name__ == '__main__':
+    subprocess.call('clear', shell=True)
+    remoteServer = resolve(sys.argv[1] if len(sys.argv) > 1 else input("Enter a host to scan: "))
+    print("wait, scanning host ports", remoteServer, end="\n\n")
+    scan(remoteServer)
+
